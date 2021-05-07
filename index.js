9859c5476ca54661a1b2c35fce7ff97e3f071cbe
Anuradha Abhaya <anuradha.abhaya@free.fr>
2021-05-07 17:17:05 +0200

    first attempt to the bowling kata

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-function sum(a, b) {
-  return a + b;
-}
+export class Game {
+  constructor() {
+    this.totalScore = 0;
+    this.rollNumber = 0;
+    this.frame = 1;
+    this.endGame = false;
+  }
 
-module.exports = sum;
+  roll(number) {
+    this.totalScore += number;
+    this.rollNumber++;
+    if ((this.rollNumber === 2 || this.isStrike(number)) && this.frame < 10) {
+      this.goToNextFrame();
+    }
+    if (this.frame === 10) {
+      if (!this.isStrike(number)) {
+        this.endGame = true;
+      }
+    }
+  }
+
+  isStrike(number) {
+    return number === 10;
+  }
+
+  isEndGame() {
+    return this.endGame;
+  }
+
+  score() {
+    return this.totalScore;
+  }
+
+  getFrame() {
+    return this.frame;
+  }
+
+  goToNextFrame() {
+    this.frame++;
+    this.rollNumber = 0;
+  }
+}
--- index.test.js
+++ index.test.js
@@ -1,5 +1,111 @@
-const sum = require("./index");
+import { Game } from "./index";
 
-test("should return sum of two number", () => {
-  expect(sum(1, 2)).toBe(3);
+test("un lancer de 10 points", () => {
+  const game = new Game();
+  game.roll(10);
+  expect(game.score()).toEqual(10);
 });
+
+test("un lancer de 5 points", () => {
+  const game = new Game();
+  game.roll(5);
+  expect(game.score()).toEqual(5);
+});
+
+test("Deux lancés de 5 points", () => {
+  const game = new Game();
+  game.roll(5);
+  game.roll(5);
+  expect(game.score()).toEqual(10);
+});
+
+test("un lancé de 7 points et un lancé de 3", () => {
+  const game = new Game();
+  game.roll(7);
+  game.roll(3);
+  expect(game.score()).toEqual(10);
+});
+
+test("en debut de partie nous sommes au tour un", () => {
+  const game = new Game();
+  expect(game.getFrame()).toEqual(1);
+  expect(game.score()).toEqual(0);
+});
+
+test("après deux lancés nous passons au tour numéro 2", () => {
+  const game = new Game();
+  game.roll(7);
+  game.roll(3);
+  expect(game.getFrame()).toEqual(2);
+});
+
+test("après un strike je passe au tour suivant", () => {
+  const game = new Game();
+  game.roll(10);
+  expect(game.getFrame()).toEqual(2);
+});
+
+test("la partie se termine au bout de 10 tours", () => {
+  const game = new Game();
+  tenFramesGenerator(game);
+
+  expect(game.getFrame()).toEqual(10);
+  expect(game.isEndGame()).toBeTruthy();
+});
+
+test("lorsque je fais un strike au dernier tour je le droit à deux lancés", () => {
+  const game = new Game();
+
+  tenFramesAndStrikeGenerator(game);
+
+  expect(game.getFrame()).toEqual(10);
+  expect(game.isEndGame()).toBeFalsy();
+
+  game.roll(5);
+  game.roll(2);
+
+  // il n'y a rien qui bloque le fait qu'on puisse faire plus que 2 rolls
+
+  expect(game.isEndGame()).toBeTruthy();
+});
+
+/*test("lorsque je fais un spare au dernier tour je le droit à 1 lancés", () => {
+  const game = new Game();
+
+  tenFramesAndStrikeGenerator(game);
+
+  expect(game.getFrame()).toEqual(10);
+  expect(game.isEndGame()).toBeFalsy();
+
+  game.roll(5);
+  game.roll(2);
+
+  expect(game.isEndGame()).toBeTruthy();
+});*/
+
+function tenFramesAndStrikeGenerator(game) {
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+}
+
+function tenFramesGenerator(game) {
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(10);
+  game.roll(2);
+  game.roll(3);
+}
